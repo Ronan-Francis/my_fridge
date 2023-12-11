c50793258666573a89377022f7c686b9af42a024
Ronan-Francis <ronananthonyfrancis@gmail.com>
2023-12-11 12:15:31 +0000

    Added Stle to RecipeForm

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+
 const RecipeForm = () => {
     const [name, setName] = useState('');
     const [ingredients, setIngredients] = useState([]);
